18db132acff8705292e3cf7a94b3f1e4baefa39a
arnaudbouffard <arn0n0@hotmail.com>
2014-04-20 09:09:25 +0200

    accelerating a little bit

--- 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
@@ -1,23 +1,25 @@
-require_relative "compute_name"
+require_relative 'compute_name'
 
 def ask_and_get(param)
-  #TODO:
-  #- Ask the user to write a value for param
+  # TODO: - Ask the user to write a value for param
   #- Get and return the string inputted by the user
+  puts "Howdie pirate, whot's yer #{param}?"
+  gets.chomp
 end
 
 def name_from_terminal
-  first_name = ask_and_get("first name")
-  second_name = ask_and_get("middle name")
-  last_name = ask_and_get("last name")
+  first_name = ask_and_get('first name')
+  second_name = ask_and_get('middle name')
+  last_name = ask_and_get('last name')
 
   compute_name(first_name, second_name, last_name)
 end
 
 full_name = name_from_terminal
 
-#TODO: use string concatenation to build a custom message embedding the computed full_name
+# TODO: use string concatenation to build a custom message embedding the
+# computed full_name
 
-# custom_message =
+custom_message = "#{full_name}, ya betta work ya ass off to catch the ouagon!"
 
 puts custom_message
