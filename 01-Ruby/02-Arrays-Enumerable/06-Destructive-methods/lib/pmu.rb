ea76e080111ad30ab96ab1f5aee3e4121b058973
arnaudbouffard <arn0n0@hotmail.com>
2014-04-20 23:37:08 +0200

    pmu anchorman riqles ready

--- 01-Ruby/02-Arrays-Enumerable/06-Destructive-methods/lib/pmu.rb
+++ 01-Ruby/02-Arrays-Enumerable/06-Destructive-methods/lib/pmu.rb
@@ -1,7 +1,8 @@
 def pmu_format!(race_array)
-  #TODO: modify the given array so that it is PMU-consistent
+  # TODO: modify the given array so that it is PMU-consistent
+  nb_horses = race_array.length
+
+  race_array.reverse!
+
+  race_array.map! { |e| (nb_horses - race_array.index(e)).to_s + '-' + e + '!' }
 end
-
-
-
-
