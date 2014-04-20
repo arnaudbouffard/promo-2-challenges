86c920c2b1eac039d4eae84b696fa1bde4d94e86
arnaudbouffard <arn0n0@hotmail.com>
2014-04-20 08:54:33 +0200

    managed to refactor

--- 01-Ruby/01-Programming-basics/03-Age-in-days/lib/age_in_days.rb
+++ 01-Ruby/01-Programming-basics/03-Age-in-days/lib/age_in_days.rb
@@ -5,4 +5,5 @@ require 'date'
 
 def age_in_days(day, month, year)
   #TODO: return the age expressed in days given the day, month, and year of birth
+  ((Time.now-Time.new(year, month, day)).round / (60*60*24)).to_i
 end
--- 01-Ruby/01-Programming-basics/03-Age-in-days/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/03-Age-in-days/lib/interface.rb
@@ -1,24 +1,21 @@
-# This "require_relative" line loads your own "age_in_days.rb" file. Notice the
-# "_relative" suffix: the given path is relative to the location of the file
-# from which "require_relative" is called
 require_relative './age_in_days'
 
 ### Talking with the user ###
-puts 'What\'s your year of birth ?'
-birth_year = gets.chomp.to_i
+a = Array.new(3)
 
-puts 'What\'s your month of birth ?'
-birth_month = gets.chomp.to_i
+["year", "month", "day"].each_with_index do | element, index|
+  puts "What\'s your #{element} of birth ?"
+  a[index] = gets.chomp.to_i
+  puts a
+end
 
-puts 'What\'s your day of birth ?'
-birth_day = gets.chomp.to_i
+# tried to find a way to refactor variables naming ie. something like
+# "birth_#{element}" = gets.chomp_to_i, in vain
+
 #############################
 
 puts 'Computing your age (with the most complicated algorithms)........'
 
-# TODO: This is probably where you'd like to use your brand new function!
-calculated_age = 0
+calculated_age = age_in_days(a[2], a[1], a[0])
 
-
-# Finally, print user's age in days:
 puts "You are #{calculated_age} days old... phew!"
