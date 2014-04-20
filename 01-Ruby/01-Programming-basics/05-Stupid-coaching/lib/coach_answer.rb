0c6fe874014579e0ef3d5a26144d7b41a11c7e32
arnaudbouffard <arn0n0@hotmail.com>
2014-04-20 10:14:26 +0200

    works but rake tests fail

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,8 +1,16 @@
+# THIS SEEMS TO WORK HOWEVA RAKE TESTS FAIL
+# PLUS did not understand  "In any case this program should exit
+# after the first coach's answer." kitt's spec
+
 def coach_answer(your_message)
   # TODO: return coach answer to your_message
+  if your_message[-1] == "?" then puts "Silly question, get dressed and go to work !"
+  else puts "I don't care son, get dressed and go to work !"
+  end
 end
 
-def coach_answer_enhanced(your_message)
-  # TODO: return coach answer to your_message, with additional custom rules of yours !
-end
+# def coach_answer_enhanced(your_message)
+#   # TODO: return coach answer to your_message, with additional custom rules of yours !
+
+# end
 
--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,3 +1,8 @@
+
 require_relative "coach_answer"
 #TODO: Implement the program that makes you discuss with your coach from the terminal.
-
+a = gets.chomp
+until a == "I am going to work right now SIR !"
+  coach_answer(a)
+  a = gets.chomp
+end
