f68f003344b1aadc0295c2b44633f538d9868ced
arnaudbouffard <arn0n0@hotmail.com>
2014-04-20 21:55:09 +0200

    wtf rake failure

--- 01-Ruby/02-Arrays-Enumerable/05-Debug-Gym/lib/bugged_concatenate.rb
+++ 01-Ruby/02-Arrays-Enumerable/05-Debug-Gym/lib/bugged_concatenate.rb
@@ -1,7 +1,11 @@
-def bugged_concatenate(array)
-  array.inject("") { |output, element| output + element }.upcase
+def bugged_concatenate(a, b, c, d, e)
+  array = [] << a.to_s.upcase << b.to_s.upcase << c.to_s.upcase << d.to_s.upcase << e.to_s.upcase
+  result = array.reduce('') do |output, element|
+    output + element
+  end
+  puts result
 end
 
 def build_1984_title
-  bugged_concatenate(1, "9", 84, " ", "George Orwell")
+  bugged_concatenate(1, '9', 84, ' ', 'George Orwell')
 end
