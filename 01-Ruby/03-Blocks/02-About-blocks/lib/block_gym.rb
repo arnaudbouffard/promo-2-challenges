396e7ee3ebc8314a155f9a128782defc9b16de0a
arnaudbouffard <arn0n0@hotmail.com>
2014-04-21 00:17:27 +0200

    more work needed

--- 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
+++ 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
@@ -1,6 +1,7 @@
 def tag(tag_name, attr = nil)
   #TODO:  Build HTML tags around  content given in the block
   #       The method can be called with an optional HTML attribute inputted as [attr_name, attr_value]
+  '<'+ tag_name +'>' + yield + '</'+ tag_name +'>'
 end
 
 def timer_for
