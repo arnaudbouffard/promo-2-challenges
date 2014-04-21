045de3e3d298cb6f353c4700b0721540494a052a
arnaudbouffard <arn0n0@hotmail.com>
2014-04-21 10:20:45 +0200

    getters and setters ok

--- 02-OOP/02-Getters-and-Setters/lib/playboy.rb
+++ 02-OOP/02-Getters-and-Setters/lib/playboy.rb
@@ -2,6 +2,9 @@ class Playboy
 
   #TODO: implement necessary getters or setters to run meeting_casanova.rb
 
+attr_reader :nationality, :conquests
+attr_accessor :hair_length
+
   INITIAL_HAIR_LENGTH = 20  # beautiful long playboy hair
 
   def initialize(name, nationality)
@@ -9,6 +12,22 @@ class Playboy
     @name, @nationality = name, nationality
     @hair_length = INITIAL_HAIR_LENGTH
   end
+
+#  def nationality
+#    @nationality
+#  end
+#
+#  def conquests
+#    @conquests
+#  end
+#
+#  def hair_length
+#    @hair_length
+#  end
+#
+#  def hair_length= neu_hair_length
+#      @hair_length = neu_hair_length
+#  end
 
   def meets(lady)
     @conquests << lady unless @married
