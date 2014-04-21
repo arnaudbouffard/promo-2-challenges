36bc9827c3fcb97ab2ee1b9564d691f38743a806
arnaudbouffard <arn0n0@hotmail.com>
2014-04-21 18:20:14 +0200

    rake tests ok

--- 02-OOP/06-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/06-Instance-vs-Class/lib/restaurant.rb
@@ -1,11 +1,23 @@
+# blabla
 class Restaurant
-  
-  #TODO add relevant accessors if necessary
-  
+  # TODO: add relevant accessors if necessary
+  attr_reader :average_rating, :city
+
   def initialize(city, name)
-    #TODO: implement constructor with relevant instance variables
+    # TODO: implement constructor with relevant instance variables
+    @city = city
+    @ratings = []
+    @average_rating = 0
+    @name = name
   end
-  
-  #TODO: implement #filter_by_city and #rate methods
-  
+
+  # TODO: implement #filter_by_city and #rate methods
+  def self.filter_by_city(restaurants, queried_city)
+    restaurants.select { |restaurant| restaurant.city == queried_city }
+  end
+
+  def rate(rating)
+    @ratings << rating
+    @average_rating = @ratings.reduce(:+) / @ratings.length
+  end
 end
