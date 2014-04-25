59ef4bc03e580a0ba9902ab4d10eee69641634b7
arnaudbouffard <arn0n0@hotmail.com>
2014-04-25 17:43:00 +0200

    top 5 rockstars

--- 03-AR-Database/04-SQL-queries/lib/join_queries.rb
+++ 03-AR-Database/04-SQL-queries/lib/join_queries.rb
@@ -1,22 +1,22 @@
 require 'sqlite3'
 
 # opens the database
-DATABASE_PATH = "db/jukebox.sqlite"
+DATABASE_PATH = 'db/jukebox.sqlite'
 db = SQLite3::Database.new(DATABASE_PATH)
 
 def divider
-  puts "\n ----------------------------------------------------------------------------------------"
+  puts '\n ----------------------------------------------------------------------------------------'
 end
 
 3.times do divider end
 
-  def detailed_tracks(db)
+def detailed_tracks(db)
   # TODO: return the list of tracks with their album and artist
-  db.execute( "SELECT Track.Name, Artist.Name, Album.Title FROM Track
+  db.execute( 'SELECT Track.Name, Artist.Name, Album.Title FROM Track
     JOIN Album ON Track.AlbumID = Album.AlbumID
-    JOIN Artist ON Album.ArtistID = Artist.ArtistID") do |row|
-  p row
-end
+    JOIN Artist ON Album.ArtistID = Artist.ArtistID') do |row|
+    p row
+  end
 end
 
 def mill_to_min(mill)
@@ -38,7 +38,7 @@ end
 
 def top_five_rock_artists(db)
   #TODO: return list of top 5 rock artists
-  db.execute( "SELECT Artist.Name, Count(Track.TrackID) AS NumberTracks FROM Artist
+  db.execute( 'SELECT Artist.Name, Count(Track.TrackID) AS NumberTracks FROM Artist
     JOIN Album ON Artist.ArtistId = Album.ArtistId
     JOIN Track ON Album.AlbumId = Track.AlbumId
     JOIN Genre ON Genre.GenreId = Track.GenreId
@@ -46,17 +46,10 @@ def top_five_rock_artists(db)
     GROUP BY Artist.Name
     ORDER BY NumberTracks desc
     LIMIT 5
-    " ) do |row|
+    ' ) do |row|
   p row
 end
-#join
-#Count
-#group by
-#order by
-#limit#
 
-end
-
-#detailed_tracks(db)
-# stats_on(db, "Rock")
+# detailed_tracks(db)
+# stats_on(db, 'Rock')
 # top_five_rock_artists(db)
