3579fa5531ded17c814b82019f54d7ad54b31440
arnaudbouffard <arn0n0@hotmail.com>
2014-04-28 14:15:17 +0200

    basic features only

--- 03-AR-Database/05-SQL-CRUD/lib/crud.rb
+++ 03-AR-Database/05-SQL-CRUD/lib/crud.rb
@@ -2,17 +2,38 @@ require 'sqlite3'
 
 def create_scheme(db)
   #TODO: create your db scheme
-  sql = %q{ 
-  }  # %q allows for multiline string in ruby 
+  sql = %q{ CREATE TABLE posts(
+    name VARCHAR,
+    rating INTEGER,
+    source_url VARCHAR,
+    date
+  )}  # %q allows for multiline string in ruby -SINGLE QUOTE
   db.execute(sql)
 end
 
 def create_post(db, post)
   #TODO: add a new post to your db
+  sql = %Q{ INSERT INTO posts(name, rating, source_url, date) VALUES(
+    '#{post[:name]}',
+    '#{post[:rating]}',
+    '#{post[:source_url]}',
+    '#{post[:date]}'
+  )}
+  db.execute(sql)
+
+# %Q allows for multiline string in ruby -DOUBLE QUOTE
+
 end
 
 def get_posts(db)
    #TODO: list all posts
+   sql = %Q{ SELECT  * FROM posts
+  }
+  puts "\n"
+  db.execute(sql) do |row|
+    p row
+  end
+  puts "\n"
 end
 
 def get_post(db, id)
@@ -24,7 +45,10 @@ def update_post(db, id, name)
 end
 
 def delete_posts(db)
-  #TODO: delete a post in your db
+  #TODO: delete all posts in your db
+  sql = %Q{ DELETE FROM posts}
+  db.execute(sql)
+  puts "\n All posts have been deleted \n"
 end
 
 def delete_post(db, id)
