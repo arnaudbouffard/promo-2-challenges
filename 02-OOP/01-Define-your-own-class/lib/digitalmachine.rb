59b0a092d931f6f5830ed50f43262705e718dde9
arnaudbouffard <arn0n0@hotmail.com>
2014-05-01 11:01:57 +0200

    working directory now clean

--- 02-OOP/01-Define-your-own-class/lib/digitalmachine.rb
+++
@@ -1,7 +0,0 @@
-# my first class!
-class DigitalMachine
-  attr_accessor :family,:technology, :openhardware, :maker, :name
-  def initialize(family, technology, openhardware, maker, name)
-    @family = @technology = @openhardware = @maker = @name = ''
-  end
-end
