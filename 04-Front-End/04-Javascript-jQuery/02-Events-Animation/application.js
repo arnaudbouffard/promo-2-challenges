2e6faff32c106d3b895c2bb599d7a972bbd63d32
arnaudbouffard <arn0n0@hotmail.com>
2014-05-05 16:41:02 +0200

    did not understand msg-wrapper trick

--- 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
+++ 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
@@ -3,3 +3,10 @@
   * TODO: add your code here!
   *
 */
+$('#message-wrapper').animate({ top: '-=70'},0);
+$('#message-wrapper').delay(500).animate({ top: '+=70'},1000);
+$('#message-wrapper').on('click', function(e) {
+
+ $(this).animate({ top: '-=70'},1000);
+
+});
