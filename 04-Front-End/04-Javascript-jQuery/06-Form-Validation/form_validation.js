b85c484efefa153a668ac90056f887693f3da074
arnaudbouffard <arn0n0@hotmail.com>
2014-05-09 12:50:16 +0200

    button enabling finally ok, now need to implement fields validation

--- 04-Front-End/04-Javascript-jQuery/06-Form-Validation/form_validation.js
+++ 04-Front-End/04-Javascript-jQuery/06-Form-Validation/form_validation.js
@@ -1,3 +1,98 @@
-// Your validation code will go in there.
-// Write your validation functions, and bind events
-// in a jQuery DOM ready callback
+var nbOfValidAnswers = 0;
+var questions = 9
+
+
+function testFormValidity(e) {
+  console.log('inside testFormValidity');
+  updateOkClassStatus(e);
+};
+
+function updateOkClassStatus(e) {
+  if ( testCellValidity($(e.target)) == true ) {
+    // greenIt($(this));
+    $(e.target).addClass('OK');
+    toggleSubmitState('OK');
+  } else {
+    // redIt($(this));
+    $(e.target).removeClass('OK');
+    toggleSubmitState('WRONG');
+  };
+};
+
+
+function countFieldsOk() {
+  return $('.OK').size();
+};
+
+
+function testCellValidity(target) {
+  console.log('inside testCellValidity, just focused out of ' + target.attr('id') + ' field');
+
+  var fieldId = target.attr('id');
+  console.log('FIELD ID is ' + fieldId);
+  switch (fieldId) {
+    case 'tos':
+      console.log('checkbox is checked ' + target.is(':checked')); //target.attr("checked"));
+      if ( target.is(':checked') == true) {
+        return true;
+      }else{
+        return false;
+      };
+    break;
+    default:
+      if (target.val() !== '') {
+        return true;
+      };
+      return false;
+  }
+
+
+
+
+};
+
+
+function toggleSubmitState(exitedField) {
+  console.log('inside toggleSubmitState');
+
+  if ( exitedField == 'OK' ) {
+    console.log('field we exited is OK, nb of OKs = ' + countFieldsOk() );
+    if ( countFieldsOk() == questions) {
+      console.log('ENABLING BUTTON!!!');
+      $('.btn-primary').prop("disabled", false);
+    };
+  } else { if ( exitedField == 'WRONG' ) {
+    console.log('field we exited is WRONG, nb of OKs = ' + countFieldsOk() );
+    if ( countFieldsOk() !== questions) {
+      console.log('DISABLING BUTTON!!!');
+      $('.btn-primary').prop("disabled", true);
+    };
+  } else {
+    console.log('wrong argument for toggleSubmitState');
+  };
+};
+};
+
+
+
+
+
+// ******** DOCUMENT READY *******
+
+
+
+
+$(document).ready(function() {
+
+
+
+  $('input').on('focusout', testFormValidity);
+  $('textarea').on('focusout', testFormValidity);
+  $('form').on('submit', function() {
+    event.preventDefault();
+    alert('Form can now be submitted!');
+
+  });
+
+
+});
